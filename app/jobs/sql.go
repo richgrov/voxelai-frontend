28c95ed0a253321eba91af4c155dbfd1b058dbae
unknown <38992201+richgrov@users.noreply.github.com>
2023-11-28 15:27:17 -0700

    Properly return error from sql service

--- app/jobs/sql.go
+++ app/jobs/sql.go
@@ -17,7 +17,7 @@ func NewSqlServce(dbUrl string) (*SqlJobsService, error) {
 
 	_, err = db.Exec("CREATE TABLE IF NOT EXISTS jobs (id text NOT NULL PRIMARY KEY, prompt text NOT NULL, status text, result text)")
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	return &SqlJobsService{
